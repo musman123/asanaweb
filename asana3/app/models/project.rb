39510ed19b167efd03132321b2a0b0838b5ea300
musman123 <musman2015@namal.edu.pk>
2020-03-29 22:23:40 +0500

    Update project.rb

--- asana3/app/models/project.rb
+++ asana3/app/models/project.rb
@@ -2,5 +2,4 @@ class Project < ActiveRecord::Base
     has_and_belongs_to_many :tasks
     has_one   :picture, as: :pictureable
     accepts_nested_attributes_for :picture ,reject_if: :all_blank , allow_destroy: true
-    accepts_nested_attributes_for :tasks ,reject_if: :all_blank, allow_destroy: true
 end
