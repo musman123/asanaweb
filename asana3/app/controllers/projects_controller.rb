fef4ad17732f3e3b34961495568e804847962c5e
musman123 <musman2015@namal.edu.pk>
2020-03-29 22:10:48 +0500

    Update projects_controller.rb

--- asana3/app/controllers/projects_controller.rb
+++ asana3/app/controllers/projects_controller.rb
@@ -3,38 +3,45 @@ class ProjectsController < ApplicationController
   def index
         @projects = Project.order(:id)
       end
-    def new
+
+      def new
         @project = Project.new
         @project.build_picture
       end
+
       def show
         @project = Project.find(params[:id])
-        @task.build_picture
       end
+
       def create
-        @project = Project.new(project_params)
-        @project.save
-        redirect_to @project
+         @project = Project.new(project_params)
+        if @project.save
+         redirect_to @project , notice: 'Project is created.'
+        else
+        render :new, notice: 'Project not created.'
+        end
       end
+
       def edit
         @project = Project.find(params[:id])   
       end
+
       def update 
         @project = Project.find(params[:id])
         if @project.update(project_params)
-          redirect_to @project
+          redirect_to @project, notice: 'Project is updated.'
         else
-          render 'edit'
+          render :edit, notice: 'Project not updated.'
         end
       end
+
       def destroy
         @project = Project.find(params[:id])
         @project.destroy
-        redirect_to projects_path
+        redirect_to projects_path, notice: 'Project Is Destroyed.'
       end
     
-      private
-        def project_params
-          params.require(:project).permit(:title, :description, :department, picture_attributes: [:id, :photo])
-        end
+      def project_params
+        params.require(:project).permit(:task_id, :title, :description, :department, picture_attributes: [:id, :photo])
+      end
 end
