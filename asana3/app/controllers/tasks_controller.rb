9f5d53d5d7361adec28ba5e2b55d09880399cc8a
musman123 <musman2015@namal.edu.pk>
2020-03-29 22:15:23 +0500

    Update tasks_controller.rb

--- asana3/app/controllers/tasks_controller.rb
+++ asana3/app/controllers/tasks_controller.rb
@@ -1,17 +1,20 @@
 class TasksController < ApplicationController
  
- def index
-    @tasks = Task.order(:id)
-  end
-  def show
-    @task = Task.find(params[:id])
-  end
-    def new
+    def index
+         @tasks = Task.order(:id)
+      end
+
+      def show
+        @task = Task.find(params[:id])
+      end
+
+      def new
         @task = Task.new
         @task.comments.build
         @task.projects.build
         @task.build_picture
       end
+
       def create   
         @task = Task.new(task_params)   
           if @task.save
@@ -19,40 +22,39 @@ class TasksController < ApplicationController
             @user = User.find(assigner)
             assigner = @user.name   
             @task.update_attribute(:assigner, assigner)
-            @project = params[:project][:project_id]
-            @task.project_ids = @project
-            redirect_to @task
+            redirect_to @task, notice: 'Task is created.'
           else 
-            flash[:error] = "Project has not been created due to some error"
-            render 'new'
+            render :new, notice: 'Task is not Created.' 
           end
-        end
+      end
      
       def edit
         @task = Task.find(params[:id])
       end
+
       def update 
         @task = Task.find(params[:id])
-        if @task.update(task_params)
-          @project = params[:project][:project_id]
-          @task.project_ids = @project
-          redirect_to @task
+         if @task.update(task_params)
+           redirect_to @task ,notice: 'Task  Successfully updated.'
         else
-          render 'edit'
+          render :edit, notice: 'Task is not Updated.' 
         end
       end
       
       def destroy
         @task = Task.find(params[:id])
         @task.destroy
-        redirect_to tasks_path
+        redirect_to tasks_path notice: 'Task successfully Destroyed .'
       end
+
        def task_params
-        params.require(:task).permit(:id, :name, :task_description, :assigner, :assign_to, :all_project, :project_name, :project_ids,
+        params.require(:task).permit(:id, :name, :task_description, :assigner, :assign_to, :user_id, :photo,
         projects_attributes: [:id, :title, :description],
         comments_attributes: [:id, :commenter, :body, :_destroy],
         picture_attributes: [:id, :photo],
-        user_attributes: [:id, :name])
+        user_attributes: [:id, :email],
+        project_ids: []
+        )
       end
        
 end
