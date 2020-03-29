4537b744e3d592fcc3ebf75a30a0557f73379ad0
musman123 <musman2015@namal.edu.pk>
2020-03-29 22:09:23 +0500

    Update comments_controller.rb

--- asana3/app/controllers/comments_controller.rb
+++ asana3/app/controllers/comments_controller.rb
@@ -7,13 +7,31 @@ class CommentsController < ApplicationController
         @comment = Comment.new
         @comment.build_picture
     end
+
     def create
         @comment = Comment.new(comment_params)  
-        redirect_to comment_path(@comment)
-      end
-     
-      private
-        def comment_params
-          params.require(:comment).permit(:commenter, :body, picture_attributes: [:id, :photo])
+        if @comment.save
+        redirect_to comment_path(@comment),notice: 'Comment is successfully created.'
+        else
+        @comment.errors  notice: 'Comment not created.'
+            render 'new'
         end
+
+    end
+    def update 
+        @comment = Comment.find(params[:id])
+         if @comment.update(comment_params)
+           redirect_to @comment ,notice: 'Comment Successfully updated.'
+        else
+          @comment.errors notice: 'Comment is not updated.'
+        end
+      end
+
+    def set_comment
+        @comment = Comment.find(params[:id])
+    end
+      # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+        params.require(:comment).permit(:task_id, :commenter, :body, picture_attributes: [:id, :photo])
+    end
 end
