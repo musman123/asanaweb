7b96a6c1ad21e871a21ac98f12be561503b59a9b
musman123 <musman2015@namal.edu.pk>
2020-03-29 22:25:20 +0500

    Update user.rb

--- asana3/app/models/user.rb
+++ asana3/app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
     validates :name, presence: true 
     validates :email, presence: true,  uniqueness: true       
-    has_many  :tasks, dependent: :destroy
+    has_many  :tasks
     has_one   :picture, as: :pictureable
-    accepts_nested_attributes_for :picture  
+    accepts_nested_attributes_for :picture 
+    accepts_nested_attributes_for :tasks ,reject_if: :all_blank , allow_destroy: true
     attr_accessor :password, :password_conformation
 end
