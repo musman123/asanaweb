fa18f1f8616b1bb7319ef30134dd980bde40ec54
musman123 <musman2015@namal.edu.pk>
2020-03-30 18:02:28 +0500

    Update comments_controller.rb

--- asana3/app/controllers/comments_controller.rb
+++ asana3/app/controllers/comments_controller.rb
@@ -1,6 +1,11 @@
 class CommentsController < ApplicationController
+
     def index
         @comments = Comment.all
+    end
+
+    def show
+        @comment = Comment.find(params[:id])
     end
 
     def new
@@ -8,30 +13,39 @@ class CommentsController < ApplicationController
         @comment.build_picture
     end
 
+    def edit
+        @comment = Comment.find(params[:id])
+    end
+
     def create
         @comment = Comment.new(comment_params)  
+
         if @comment.save
-        redirect_to comment_path(@comment),notice: 'Comment is successfully created.'
+            redirect_to comment_path(@comment),notice: 'Comment is successfully created.'
         else
-        @comment.errors  notice: 'Comment not created.'
-            render 'new'
+            render :new, notice: 'Comment not created.'    
         end
 
     end
+
     def update 
         @comment = Comment.find(params[:id])
-         if @comment.update(comment_params)
+
+        if @comment.update(comment_params)
            redirect_to @comment ,notice: 'Comment Successfully updated.'
         else
-          @comment.errors notice: 'Comment is not updated.'
+           render :edit, notice: 'Comment is not updated.'
         end
-      end
+
+    end
 
     def set_comment
         @comment = Comment.find(params[:id])
     end
+
       # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
         params.require(:comment).permit(:task_id, :commenter, :body, picture_attributes: [:id, :photo])
     end
+
 end
