b8b999d6fff8b3920ac77aa14233a901ce907106
musman123 <musman2015@namal.edu.pk>
2020-03-30 18:05:02 +0500

    Update sessions_controller.rb

--- asana3/app/controllers/sessions_controller.rb
+++ asana3/app/controllers/sessions_controller.rb
@@ -1,23 +1,25 @@
 class SessionsController < ApplicationController
   skip_before_action :authorized, only: [:new, :create, :welcome]
-  
-  def new   
 
-  end   
+  def new
 
-  def create    
-   user = User.find_by(password: params[:password],email: params[:email])
-    if user
-      session[:user_id] = user.id   
-      redirect_to root_url, notice: 'Logged in!'   
-    else   
-      render :new   
-    end   
-  end   
+  end
 
-  def destroy   
-    session[:user_id] = nil   
-    redirect_to root_url, notice: 'Logged out!'   
-  end   
+  def create
+    user = User.find_by(password: params[:password],email: params[:email])
+
+      if user
+        session[:user_id] = user.id
+        redirect_to root_url, notice: 'Logged in!'
+      else
+        render :new
+      end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
+  end
 
 end
