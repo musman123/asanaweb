0393b39cc71370198414c1c9150c3711dad0c6c0
musman123 <musman2015@namal.edu.pk>
2020-03-30 18:09:37 +0500

    Update comment.rb

--- asana3/app/models/comment.rb
+++ asana3/app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  belongs_to :task 
-  has_one   :picture, as: :pictureable
-  accepts_nested_attributes_for :picture ,reject_if: :all_blank , allow_destroy: true
+    belongs_to :task 
+    has_one   :picture, as: :pictureable
+    accepts_nested_attributes_for :picture ,reject_if: :all_blank , allow_destroy: true 
 end
