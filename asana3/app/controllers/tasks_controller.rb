d8bf223082ca07d577902e7a4f6d5067a6b1d4f3
musman123 <musman2015@namal.edu.pk>
2020-03-30 18:08:04 +0500

    Update tasks_controller.rb

--- asana3/app/controllers/tasks_controller.rb
+++ asana3/app/controllers/tasks_controller.rb
@@ -1,60 +1,63 @@
 class TasksController < ApplicationController
- 
-    def index
-         @tasks = Task.order(:id)
-      end
 
-      def show
-        @task = Task.find(params[:id])
-      end
+   def index
+     @tasks = Task.order(:id)
+   end
 
-      def new
-        @task = Task.new
-        @task.comments.build
-        @task.projects.build
-        @task.build_picture
-      end
+   def show
+     @task = Task.find(params[:id])
+   end
 
-      def create   
-        @task = Task.new(task_params)   
-          if @task.save
-            assigner = session[:user_id]
-            @user = User.find(assigner)
-            assigner = @user.name   
-            @task.update_attribute(:assigner, assigner)
-            redirect_to @task, notice: 'Task is created.'
-          else 
-            render :new, notice: 'Task is not Created.' 
-          end
-      end
-     
-      def edit
-        @task = Task.find(params[:id])
-      end
+   def new
+     @task = Task.new
+     @task.comments.build
+     @task.projects.build
+     @task.build_picture
+   end
 
-      def update 
-        @task = Task.find(params[:id])
-         if @task.update(task_params)
-           redirect_to @task ,notice: 'Task  Successfully updated.'
-        else
-          render :edit, notice: 'Task is not Updated.' 
-        end
-      end
-      
-      def destroy
-        @task = Task.find(params[:id])
-        @task.destroy
-        redirect_to tasks_path notice: 'Task successfully Destroyed .'
-      end
+   def edit
+     @task = Task.find(params[:id])
+   end
 
-       def task_params
-        params.require(:task).permit(:id, :name, :task_description, :assigner, :assign_to, :user_id, :photo,
-        projects_attributes: [:id, :title, :description],
-        comments_attributes: [:id, :commenter, :body, :_destroy],
-        picture_attributes: [:id, :photo],
-        user_attributes: [:id, :email],
-        project_ids: []
-        )
-      end
-       
+   def create
+     @task = Task.new(task_params)
+
+       if @task.save
+          assigner = session[:user_id]
+          @user = User.find(assigner)
+          assigner = @user.name
+          @task.update_attribute(:assigner, assigner)
+          redirect_to @task, notice: 'Task is created.'
+       else
+          render :new, notice: 'Task is not Created.'
+       end
+
+   end
+
+   def update
+     @task = Task.find(params[:id])
+
+       if @task.update(task_params)
+           redirect_to @task, notice: 'Task  Successfully updated.'
+       else
+           render :edit, notice: 'Task is not Updated.'
+       end
+         
+   end
+
+   def destroy
+     @task = Task.find(params[:id])
+     @task.destroy
+     redirect_to tasks_path notice: 'Task successfully Destroyed .'
+   end
+
+   def task_params
+     params.require(:task).permit(:id, :name, :task_description, :assigner, :assign_to, :user_id,
+     projects_attributes: [:id, :title, :description],
+     comments_attributes: [:id, :commenter, :body, :_destroy],
+     picture_attributes: [:id, :photo],
+     user_attributes: [:id, :email],
+     project_ids: [] )
+   end
+
 end
