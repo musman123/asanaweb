e04e1a30fd07709b8c0f2560641aacc233a2e16e
musman123 <musman2015@namal.edu.pk>
2020-03-30 18:11:31 +0500

    Update task.rb

--- asana3/app/models/task.rb
+++ asana3/app/models/task.rb
@@ -1,10 +1,10 @@
 class Task < ActiveRecord::Base
-    has_and_belongs_to_many :projects
-    belongs_to :user
-    has_many :comments, dependent: :destroy
-    accepts_nested_attributes_for :projects ,reject_if: :all_blank, allow_destroy: true
-    accepts_nested_attributes_for :comments ,reject_if: :all_blank, allow_destroy: true
-    has_one   :picture, as: :pictureable
-    accepts_nested_attributes_for :picture ,reject_if: :all_blank , allow_destroy: true
-    accepts_nested_attributes_for :user ,reject_if: :all_blank , allow_destroy: true 
+     has_and_belongs_to_many :projects
+     belongs_to :user
+     has_many :comments, dependent: :destroy
+     accepts_nested_attributes_for :projects ,reject_if: :all_blank, allow_destroy: true
+     accepts_nested_attributes_for :comments ,reject_if: :all_blank, allow_destroy: true
+     has_one   :picture, as: :pictureable
+     accepts_nested_attributes_for :picture ,reject_if: :all_blank , allow_destroy: true
+     accepts_nested_attributes_for :user ,reject_if: :all_blank , allow_destroy: true 
 end
