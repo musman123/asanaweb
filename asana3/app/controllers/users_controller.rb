cede3025ea754bcbba035d78a907668446426e11
musman123 <musman2015@namal.edu.pk>
2020-03-30 18:08:46 +0500

    Update users_controller.rb

--- asana3/app/controllers/users_controller.rb
+++ asana3/app/controllers/users_controller.rb
@@ -1,73 +1,77 @@
 class UsersController < ApplicationController
-    skip_before_action :authorized, only: [:new, :create]
+  skip_before_action :authorized, only: [:new, :create]
      
-      def index
-         @users = User.order(:id)
-      end
+  def index
+    @users = User.order(:id)
+  end
       
-      def new
-          @user = User.new
-          @user.build_picture
-      end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+    @user.build_picture
+  end
       
-      def show
-          @user = User.find(params[:id])
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create   
+    @user = User.new(user_params)   
+    password = @user.password
+    password_conformation = @user.password_conformation
+      if(password.eql? password_conformation)
+          if @user.save   
+              session[:user_id] = @user.id 
+              redirect_to root_url, notice: 'User successfully created.'  
+          else   
+              render :new, notice: 'User is not created.'
+          end 
+
+      else 
+        render :new, notice: 'Password not Matched' 
       end
 
-      def create   
-          @user = User.new(user_params)   
+  end  
+
+  def update 
+    @user = User.find(params[:id])
+      if @user.update(user_params)
           password = @user.password
           password_conformation = @user.password_conformation
-          if(password.eql? password_conformation)
-              if @user.save   
-                session[:user_id] = @user.id 
-                redirect_to root_url, notice: 'User successfully created.'  
-              else   
-                render :new, notice: 'User is not created.'
-              end 
-          else 
-            render :new, notice: 'Password not Matched' 
-          end
-      end  
+            if(password.eql? password_conformation)
+                redirect_to @user , notice: 'User successfully updated.'
+            else
+                render :edit,  notice: 'User Not Updated Matched.' 
+            end
 
-      def edit
-         @user = User.find(params[:id])
-      end
-
-      def update 
-          @user = User.find(params[:id])
-           if @user.update(user_params)
-              password = @user.password
-              password_conformation = @user.password_conformation
-               if(password.eql? password_conformation)
-                   redirect_to @user , notice: 'User successfully updated.'
-              else
-                   render :edit,  notice: 'User Not Updated Matched.' 
-              end
-        else 
+      else 
           render :edit,  notice: 'Password Not Matched.'     
-        end
       end
 
-      def selected
-          assigned = params[:user]
-          tasker = params[:taskid]
-          @task = Task.find(tasker.to_i)
-          @user = User.find(assigned)
-          id = @user.id
-          @task.user_id = id
-          @task.save
-      end
+  end
 
-      def destroy
-          @user = User.find(params[:id])
-          @user.destroy
-          redirect_to users_path
-      end
+  def selected
+      assigned = params[:user]
+      tasker = params[:taskid]
+      @task = Task.find(tasker.to_i)
+      @user = User.find(assigned)
+      id = @user.id
+      @task.user_id = id
+      @task.save
+  end
 
-      private
-        def user_params
-          params.require(:user).permit(:task_id, :name, :email, :password, :password_conformation, picture_attributes: [:id, :photo]
-          )
-        end
+  def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to users_path
+  end
+
+  def user_params
+      params.require(:user).permit(:task_id, :name, :email, :password, :password_conformation,
+      picture_attributes: [:id, :photo])
+  end
+  
 end
